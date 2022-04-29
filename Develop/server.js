2a62c4d34cff23b9247106b2274b35e65a9288f1
Ryan Saldaen <ryan.saldaen@gmail.com>
2022-04-29 05:22:17 -0700

    server able to start with functioning first page, tried some stuff and second page is not quite what I thought it would be, need to fix.

---
+++ Develop/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+
+const PORT = 3001;
+const app = express();
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("./Develop/public/assets", express.static("./Develop/public/assets"));
+
+require('./routes/html-routes')(app);
+require('./routes/api-routes')(app);
+
+app.listen(PORT, () =>
+    console.log(`App listening on ${PORT}`)
+);
