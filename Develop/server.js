723a515bc4104efccd7d0ea16b5b7c4dcf4e2614
Ryan Saldaen <ryan.saldaen@gmail.com>
2022-06-02 02:15:11 -0700

    trying to fix heroku deploy

--- Develop/server.js
+++ Develop/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const path = require('path');
 
-const port = process.env.PORT || 3001
-const PORT = 3001;
+const PORT = process.env.PORT || 3001
 const app = express();
 
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use("./Develop/public/assets", express.static("./Develop/public/assets"));
+app.use(express.static("public"));
 
 require('./routes/html-routes')(app);
 require('./routes/api-routes')(app);
